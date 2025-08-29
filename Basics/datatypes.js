2f2f59513ef91ebf647a8e7bccad101592be0ede
Ansh Patel : rp342143@gmail.com <rp342143@gmail.com>
2025-08-29 09:23:01 -0500

    second commit

---
+++ Basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all Js code as a newer version
+
+"alert(3 + 3 )" // we are uding node js , not Browser
+
+console.log( 3 + 3 )
+
+console.log("anshpatelR.")
+
+let name = "ansh"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+// object
+
+console.log(typeof "ansh");
+console.log(typeof undefined); // undefined
+console.log(typeof age);
+console.log(typeof null); // object
+
---
+++ Basics/variable.js
@@ -0,0 +1,12 @@
+const accountId = 8448848
+let accountEmail = "ansh123@gmail.com"
+var accountPassword = "123123"
+// accountId = 2 // not allowed
+
+accountEmail = "ansh1@gmail.com"
+accountPassword = "87484"
+accountCity = "Visnagar"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
