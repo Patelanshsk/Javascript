63b6daf7338b81c944a77b9577ff57c6261588cb
Ansh Patel : rp342143@gmail.com <rp342143@gmail.com>
2025-08-29 10:20:46 -0500

    conversion operation

---
+++ Basics/conversion.js
@@ -0,0 +1,30 @@
+let score = false
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33"=> 33
+// "33abx" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// For boolean 
+// 1 => true; 0 => false 
+// "" => false
+// "Ansh" => true
+
+// For String
+
+let someNumber = 47
+
+let StringNumber = String(someNumber)
+console.log(typeof StringNumber);
+
