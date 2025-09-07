ab9002b748268c531528b0206d7d6a8b47b31fcb
Ansh Patel : rp342143@gmail.com <rp342143@gmail.com>
2025-09-07 18:39:42 -0500

    Comparision

--- 01_Basics/04_comparision.js
+++ 01_Basics/04_comparision.js
@@ -7,7 +7,7 @@ console.log(2 != 1);
 console.log("2" > 1);
 console.log("02" > 1);
 
-// Note: This type of convertion is confusing so we prefer clean code.
+// Note:- This type of convertion is confusing so we prefer clean code.
 
 // console.log(null > 0);
 // console.log(null == 0);
