8df01558d4d063d3aed511e5b34b46ba4f98cbcd
Ansh Patel : rp342143@gmail.com <rp342143@gmail.com>
2025-09-20 14:45:03 -0500

    Switch in js

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,52 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// } // Syntax of switch
+
+// Example:-
+
+// const month = 3
+const month = "march"
+
+// switch (month) {
+//     case 1: 
+//         console.log("jenuary");
+//         break;
+//     case 2: 
+//         console.log("February");
+//         break;
+//     case 3: 
+//         console.log("March");
+//         break;
+//     case 4: 
+//         console.log("April");
+//         break;
+
+//     default:
+//         console.log("Unexpected");
+        
+//         break;
+// }
+switch (month) {
+    case "jan": 
+        console.log("jenuary");
+        break;
+    case "Feb": 
+        console.log("February");
+        break;
+    case "march": 
+        console.log("March");
+        break;
+    case "Apr": 
+        console.log("April");
+        break;
+
+    default:
+        console.log("Unexpected");
+        
+        break;
+}
