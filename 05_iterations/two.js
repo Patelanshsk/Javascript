deefa956eb1f4eaac4b72175b4968e6db3503703
Ansh Patel : rp342143@gmail.com <rp342143@gmail.com>
2025-09-21 18:37:10 -0500

    While and Do-While Loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ["Superman", "Spiderman", "Thor"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`The Heroe is ${myArray[arr]}`);
+    arr = arr + 1
+    
+}
+
+// let score = 1
+let score = 12
+do {
+    console.log(`score is ${score}`);
+    // score = score + 1
+    score++
+} while (score <= 10);
